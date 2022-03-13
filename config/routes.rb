3f0a70ea7b9843b7305e10c3d3334f79ad783036
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-13 20:18:05 +0300

    Add Pages for the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'tests#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :money_tracks, only: [ :new, :show, :create]
+  end
 end
